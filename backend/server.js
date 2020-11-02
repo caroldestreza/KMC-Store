c239f76b7e1174ddd637d71bfad725a3b5ae13f9
caroldestreza <caroldestreza@github.com>
2020-11-02 23:11:16 +0800

    Updated path of API on server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import data from "./data.js";
 
 const app = express();
-app.get("api/products/:id", (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product not Found" });
+    res.status(404).send({ message: "Product Not Found" });
   }
 });
 
