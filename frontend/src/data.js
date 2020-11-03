4f9a62b9ea34d52e7f5dfddd2ef8d3e3b31b073b
caroldestreza <caroldestreza@github.com>
2020-11-03 13:19:09 +0800

    frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Armstrong Office",
+      category: "Buildings",
+      image: "/images/ArmstrongOffice.jpg",
+      price: 120,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "The Armstrong Corporate Center is KMC’s first fully dedicated building to managed offices in Salcedo Village, Makati. ",
+    },
+    {
+      _id: "2",
+      name: "Cyber Sigma",
+      category: "Buildings",
+      image: "/images/CyberSigma.jpg",
+      price: 120,
+      countInStock: 0,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Sigma Cyber Sigma is a class A office building in Lawton Avenue, Bonifacio South, Taguig City offering 42,660 sq. m. of Gross Leasable Area.",
+    },
+    {
+      _id: "3",
+      name: "Festive Walk Tower",
+      category: "Buildings",
+      image: "/images/FestiveWalkTower.jpg",
+      price: 120,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 10,
+      description:
+        "Festiva Walk Office Tower, located in Iloilo’s Mandurriao business district, provides over 12,000 sqm of leasable spaces right in the heart of the Iloilo Business Park.",
+    },
+    {
+      _id: "4",
+      name: "Four Neo",
+      category: "Buildings",
+      image: "/images/FourNeo.jpg",
+      price: 120,
+      countInStock: 0,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Located in BGC, the country's top business district, our office spaces in Four/NEO give clients a strategic position within the area.",
+    },
+    {
+      _id: "5",
+      name: "Frabelle",
+      category: "Buildings",
+      image: "/images/Frabelle.jpg",
+      price: 120,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 10,
+      description:
+        "Frabelle Corporate Plaza is a state-of-the-art, LEED-certified, sustainably designed building located at the heart of the Makati Central Business District at Salcedo village.",
+    },
+    {
+      _id: "6",
+      name: "One Griffinstone",
+      category: "Buildings",
+      image: "/images/OneGriffinstone.jpg",
+      price: 120,
+      countInStock: 0,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "One Griffinstone, located in Alabang, Muntinlupa City, is an 18-storey LEED-certified and PEZA-accredited skyscraper situated in the heart of Metro Manila’s rapidly growing business hubs.",
+    },
+  ],
+};
+
+export default data;
