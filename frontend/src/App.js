00f8dad6118ea32605fa8305182ec11ab137ff2d
caroldestreza <caroldestreza@github.com>
2020-11-06 10:35:54 +0800

    Create Register Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import { useDispatch, useSelector } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 import { signout } from "./actions/userActions";
 
 function App() {
@@ -54,6 +55,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">
